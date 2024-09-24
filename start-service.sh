723e1ef916461f0277fe6513484a2613339817d0
brianekummer <brianekummer@users.noreply.github.com>
2024-09-24 12:10:13 -0400

    Refactored start-service.sh to work on server and NUC

--- start-service.sh
+++ start-service.sh
@@ -1,3 +1,18 @@
-source /root/.env
-cd /opt/work-status/
-node server.js $SLACK_TOKENS
+case "$OSTYPE" in
+  linux-gnu*)
+    ##########################################################################
+    # Proxmox server
+    ##########################################################################
+    source /root/.env
+    cd /opt/work-status/
+    ;;
+
+  msys)
+    ##########################################################################
+    # Windows, running in Git Bash
+    ##########################################################################
+    source .env
+    ;;
+esac
+
+node server.js $SLACK_TOKENS $HOME_ASSISTANT_URL $HOME_ASSISTANT_TOKEN
