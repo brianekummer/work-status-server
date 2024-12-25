1ad1afad22c79727a629baa559028f54283909ec
Brian Kummer <brianekummer@users.noreply.github.com>
2024-12-25 10:28:35 -0500

    Cleaned up client side code

--- public/status.js
+++ public/status.js
@@ -17,14 +17,7 @@ function displaySlackStatus() {
         let mode = showDesk ? "desk" : "wall";
         document.body.className = `${visibilityClass} ${mode}`;
 
-        console.log(">>> ", jsonResponse);
-
         if (showStatus) {
-          // Get the last updated time from the header
-          const lastUpdatedTime = luxon.DateTime
-            .fromHTTP(response.headers.get('Date'))
-            .toLocaleString(luxon.DateTime.TIME_SIMPLE);
-
           if (showDesk) {
             // Set the times
             let now = luxon.DateTime.now();
@@ -37,6 +30,7 @@ function displaySlackStatus() {
 
             // Set the URL for each of the HA icons, using the HA url from the server,
             // IF it has not already been set
+            // TODO- move this out of this loop, and move building urls to server
             const baseUrl = `${jsonResponse.homeAssistant.url}/local/icon`;
             $("washer-image").src = $("washer-image").src || `${baseUrl}/mdi-washing-machine-light.png`;
             $("dryer-image").src = $("dryer-image").src || `${baseUrl}/mdi-tumble-dryer-light.png`;
@@ -47,25 +41,24 @@ function displaySlackStatus() {
             $("dryer-text").innerHTML = jsonResponse.homeAssistant.dryerText;
             $("temperature-text").innerHTML = jsonResponse.homeAssistant.temperatureText;
           }
-          else 
-          {
-            // Showing on the wall phone is the default, so no changes are needed
-          }
 
           // Load the data into the web page
           $("status-emoji").src = jsonResponse.emoji;
           $("status-text").innerHTML = jsonResponse.text;
           $("status-times").innerHTML = jsonResponse.times;
 
-          // I am intentionally not getting timestamp from the server payload because
-          // that'd cause every payload to be unique and wreck the etag caching. So
-          // instead, I'm getting the updated time from the response header.
-          $("last-updated-time").innerHTML = lastUpdatedTime;
-
-          // Shrink the status text, if necessary
+          // Set the size of the status text
           $("status-text").className = jsonResponse.text.length > 13 
             ? "status--font-size__small" 
             : "status--font-size";
+
+          // Get the last updated time from the response header. I am intentionally not 
+          // including a timestamp in the server payload because that'd cause every
+          // payload to be unique and wreck the etag caching. 
+          $("last-updated-time").innerHTML = luxon.DateTime
+            .fromHTTP(response.headers.get('Date'))
+            .toLocaleString(luxon.DateTime.TIME_SIMPLE);
+
         }
       })
       .catch(err => console.log(`ERROR: ${err}`));
